cfc179a18401b2ee1404742bf25391182eada3be
jubayer047 <akjubayer047@gmail.com>
2019-11-04 21:29:01 +0600

    change

---
+++ react/01/precache-manifest.a880b3e1640970547361867f3cfdb010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b3a1ac0c34ce0814da35c06a893a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18698881255e5808ad21",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "18698881255e5808ad21",
+    "url": "/static/js/2.f5b1746b.chunk.js"
+  },
+  {
+    "revision": "8193a926ddc314a1b740",
+    "url": "/static/js/main.de0fa280.chunk.js"
+  },
+  {
+    "revision": "1b0a0871fdc51f942174",
+    "url": "/static/js/runtime-main.a42bfb8e.js"
+  }
+]);
