bc96454d452263871181c2996c5b59329f9f1839
jubayer047 <akjubayer047@gmail.com>
2019-11-20 20:17:13 +0600

    c

---
+++ react/03/precache-manifest.ae16f26bb78efa8cd8ee453a0456aa4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7230bdcc1235b8504f0c978ef74474a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "370678a8a3de9bf76b7a",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "42c1cbf34db50bf809d5",
+    "url": "/static/css/main.a6c4296b.chunk.css"
+  },
+  {
+    "revision": "370678a8a3de9bf76b7a",
+    "url": "/static/js/2.39525d7b.chunk.js"
+  },
+  {
+    "revision": "42c1cbf34db50bf809d5",
+    "url": "/static/js/main.4c4938a4.chunk.js"
+  },
+  {
+    "revision": "413d13ef2f29e66e9534",
+    "url": "/static/js/runtime-main.c47def67.js"
+  }
+]);
