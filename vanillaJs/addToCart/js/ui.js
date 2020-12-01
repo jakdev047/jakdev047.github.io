c6026dd12e76d116f4c9c7ca0b33cb257bcec17b
jakdev047 <jakfreelancer047@gmail.com>
2020-12-01 20:51:24 +0600

    initial setup

---
+++ vanillaJs/addToCart/js/ui.js
@@ -0,0 +1,34 @@
+// Get the UI element
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// cart
+let cart = [];
+
+// getting products
+class Products {
+
+}
+
+// UI Class
+class UI {
+    constructor(){}
+}
+
+// localStorage
+class Storage {
+
+}
+
+// add event listener
+document.addEventListener('DOMContentLoaded',()=>{
+    const ui = new UI();
+    const products = new Products();
+});
