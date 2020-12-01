dba2bff8b365f6abf0d469e8d049c909462d6384
jakdev047 <jakfreelancer047@gmail.com>
2020-12-01 21:03:34 +0600

    getting products

--- vanillaJs/addToCart/js/ui.js
+++ vanillaJs/addToCart/js/ui.js
@@ -14,12 +14,27 @@ let cart = [];
 
 // getting products
 class Products {
-
+    async getProducts() {
+        try {
+            let result = await fetch('data.json');
+            let data = await result.json();
+            let products = data.items;
+            products = products.map(item => {
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image };
+            });
+            return products;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 // UI Class
 class UI {
-    constructor(){}
+    constructor() { }
 }
 
 // localStorage
@@ -28,7 +43,9 @@ class Storage {
 }
 
 // add event listener
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const ui = new UI();
     const products = new Products();
+
+    products.getProducts().then(data => console.log(data));
 });
