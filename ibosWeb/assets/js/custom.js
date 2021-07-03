bda095508697ab130a8cad36ed7077893f8391a6
jakdev047 <jakfreelancer047@gmail.com>
2021-07-03 15:46:07 +0600

    about page effect

--- ibosWeb/assets/js/custom.js
+++ ibosWeb/assets/js/custom.js
@@ -105,6 +105,51 @@ $(".return-to-top").on("click", function () {
   return false;
 });
 
+/*----------------------------
+        Mouse Move Effect
+  ------------------------------ */
+var mouse = { x: 0, y: 0 };
+
+homex = 0;
+homey = 0;
+forcex = 0;
+forcey = 0;
+magnet = 500;
+
+$(document).bind("mousemove", function (e) {
+  mouse = { x: e.pageX, y: e.pageY };
+});
+
+$(".wooble-effect").each(function (index, el) {
+  $(el).data("homex", parseInt($(el).position().left));
+  $(el).data("homey", parseInt($(el).position().top));
+});
+
+$(".wooble-effect").css("position", "absolute");
+setInterval(function () {
+  $(".wooble-effect").each(function (index, el) {
+    el = $(el);
+    position = el.position();
+    x0 = el.offset().left;
+    y0 = el.offset().top;
+    x1 = mouse.x;
+    y1 = mouse.y;
+    distancex = x1 - x0;
+    distancey = y1 - y0;
+
+    distance = Math.sqrt(distancex * distancex + distancey * distancey) + 80; //80 added by me to reduce the effect
+
+    powerx = x0 - ((distancex / distance) * magnet) / distance;
+    powery = y0 - ((distancey / distance) * magnet) / distance - 35; //-35 added by me to fix top postion
+
+    forcex = (forcex + (el.data("homex") - x0) / 2) / 2.1;
+    forcey = (forcey + (el.data("homey") - y0) / 2) / 2.1;
+
+    el.css("left", powerx + forcex);
+    el.css("top", powery + forcey);
+  });
+}, 15);
+
 $(document).ready(function () {
   "use strict";
 
